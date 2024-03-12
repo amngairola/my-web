7b026be5b7f6272a3d37aa2c1e42eed9fc765c7d
Aman Gaiola <amngairola@gmail.com>
2024-03-12 21:22:25 +0530

    about section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,29 @@ import { services } from "../constants";
 // import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full ">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card  "
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12
+          min-h-[200px] flex justify-evenly items-center flex-col
+          "
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
 const About = () => {
   return (
     <>
@@ -13,6 +36,24 @@ const About = () => {
         <p className={styles.sectionSubText}>Inroduction</p>
         <h2 className={styles.sectionHeadText}> overView</h2>
       </motion.div>
+      <motion.p
+        variants={fadeIn("", " ", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl  leading-[30px]"
+      >
+        Passionate B.Tech. Computer Science student specializing in full-stack
+        development. Experienced in front-end and back-end solutions. Proficient
+        in MERN stack – React, Node.js, Express.js, MongoDB. Skilled polyglot in
+        C, C++, Java, and JavaScript. Committed to collaboration,
+        problem-solving, and user-centric design. Actively engaged in tech
+        community, open-source projects, and continuous learning. Let's connect
+        and build something remarkable!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
     </>
   );
 };
