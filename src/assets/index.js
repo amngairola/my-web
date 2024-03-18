46a31e626a2f7dc9845c7d5ddaf3a04af32d9c62
Aman Gaiola <amngairola@gmail.com>
2024-03-19 01:06:36 +0530

    work section

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,7 @@ import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
+import Link from "./Link.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
@@ -31,6 +32,7 @@ import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
 export {
+  Link,
   logo,
   backend,
   creator,
